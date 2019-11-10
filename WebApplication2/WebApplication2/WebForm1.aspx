b7619cd0e6a9712ee7b3e76c76b9ce86c01755e3
Agustin <agustintorchia@gmail.com>
2019-11-10 19:20:11 -0300

    primer commit

---
+++ WebApplication2/WebApplication2/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        #Button1 {
+            width: 167px;
+        }
+        #Text1 {
+            width: 145px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+    <p>
+        <input id="Button1" type="button" value="button" />
+        <input id="Text1" type="text" /></p>
+</body>
+</html>
---
+++ WebApplication2/WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public ServiceReference1.ServicioEjemploSoapClient serv = new ServiceReference1.ServicioEjemploSoapClient();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            serv.HelloWorld();
+            
+            
+        }
+    }
+}
